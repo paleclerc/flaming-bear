e619112f4bf1ef9e29fb796a2c9317c74de8923a
Pierre-Alexandre Lelerc <pierrealexandre.leclerc@gmail.com>
2014-10-27 20:32:36 -0400

    Create basic project

---
+++ unity/Assets/Modules/Scripts/CameraRotation.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour 
+{
+	public GameObject m_TargetRotate;
+	public float m_SpeedRotate = 1f;
+
+	void Start()
+	{
+		UpdateRotation();
+	}
+	// Update is called once per frame
+	void Update () 
+	{
+		UpdateRotation();
+	}
+
+	void UpdateRotation ()
+	{
+		transform.LookAt(m_TargetRotate.transform);
+		transform.Translate(Vector3.right * Time.deltaTime * m_SpeedRotate);
+	}
+}
